eb7c83f4545c2a9e2b253898378f37d91135f562
Matěj Drábek <matej1drabek@seznam.cz>
2021-02-27 21:43:32 +0100

    Init game project

---
+++ game/NeuronsAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuronsAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
